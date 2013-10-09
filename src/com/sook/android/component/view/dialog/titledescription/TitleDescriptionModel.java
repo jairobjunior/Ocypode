e3011ef1edd253f31b048d2dc3ad9d1e0ac8dcf1
Jairo Junior <jairobjunior@gmail.com>
2013-10-09 15:46:00 -0700

    Implemente equals for TitleDescriptionModel class

--- src/com/sook/android/component/view/dialog/titledescription/TitleDescriptionModel.java
+++ src/com/sook/android/component/view/dialog/titledescription/TitleDescriptionModel.java
@@ -22,4 +22,35 @@ public class TitleDescriptionModel implements Serializable {
 		return mDescription;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((mDescription == null) ? 0 : mDescription.hashCode());
+		result = prime * result + ((mTitle == null) ? 0 : mTitle.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		TitleDescriptionModel other = (TitleDescriptionModel) obj;
+		if (mDescription == null) {
+			if (other.mDescription != null)
+				return false;
+		} else if (!mDescription.equals(other.mDescription))
+			return false;
+		if (mTitle == null) {
+			if (other.mTitle != null)
+				return false;
+		} else if (!mTitle.equals(other.mTitle))
+			return false;
+		return true;
+	}
 }
