ae73f6ec66c3dec2d8a007274e06ddf9ed87271c
Macksuel Santos <macksuell_10@hotmail.com>
2014-02-18 00:24:21 -0300

    adjust component Lockable

--- src/com/ocypode/component/view/listview/LockableListView.java
+++ src/com/ocypode/component/view/listview/LockableListView.java
@@ -1,16 +1,18 @@
 package com.ocypode.component.view.listview;
 
 import android.content.Context;
+import android.support.v4.view.ViewPager.OnPageChangeListener;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.widget.ListView;
 
-public class LockableListView extends ListView{
+public class LockableListView extends ListView implements OnPageChangeListener{
 	private boolean isLocked = false;
+	private boolean mScrollInit = false;
 	
 	public LockableListView(Context context, AttributeSet attrs) {
 		super(context, attrs);
-
+		
 	}
 
 	public void setLock(boolean lock){
@@ -23,6 +25,32 @@ public class LockableListView extends ListView{
 			return false;
 		}else{
 			return super.onInterceptTouchEvent(ev);			
+		}
+	}
+
+	@Override
+	public void onPageScrollStateChanged(int state) {
+		checkStateScroll(state);
+	}
+
+	@Override
+	public void onPageScrolled(int arg0, float offSet, int arg2) {
+		if (offSet > 0 && mScrollInit) {
+			setLock(true);
+		}
+	}
+
+	@Override
+	public void onPageSelected(int arg0) {
+
+	}
+	
+	private void checkStateScroll(int state){
+		if(state == 1){
+			mScrollInit = true;
+		}else if(state == 0){
+			mScrollInit = false;
+			setLock(false);
 		}
 	}
 }
--- src/com/ocypode/component/view/scrollview/LockableScrollView.java
+++ src/com/ocypode/component/view/scrollview/LockableScrollView.java
@@ -1,13 +1,14 @@
 package com.ocypode.component.view.scrollview;
 
 import android.content.Context;
+import android.support.v4.view.ViewPager.OnPageChangeListener;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.widget.ScrollView;
 
-public class LockableScrollView extends ScrollView {
-
+public class LockableScrollView extends ScrollView implements OnPageChangeListener{
 	private boolean isLocked = false;
+	private boolean mScrollInit = false;
 	
 	public LockableScrollView(Context context, AttributeSet attrs) {
 		super(context, attrs);
@@ -24,6 +25,32 @@ public class LockableScrollView extends ScrollView {
 			return false;
 		}else{
 			return super.onInterceptTouchEvent(ev);			
+		}
+	}
+	
+	@Override
+	public void onPageScrollStateChanged(int state) {
+		checkStateScroll(state);
+	}
+
+	@Override
+	public void onPageScrolled(int arg0, float offSet, int arg2) {
+		if (offSet > 0 && mScrollInit) {
+			setLock(true);
+		}
+	}
+
+	@Override
+	public void onPageSelected(int arg0) {
+
+	}
+	
+	private void checkStateScroll(int state){
+		if(state == 1){
+			mScrollInit = true;
+		}else if(state == 0){
+			mScrollInit = false;
+			setLock(false);
 		}
 	}
 }
